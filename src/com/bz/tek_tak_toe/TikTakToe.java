89e853e279df771c80abf6247bac634fb8f44aca
mudable <mudableshilpa@gmail.com>
2021-10-25 18:41:47 +0530

    Added:UC2 for determine or choose the letter for user and computer

--- src/com/bz/tek_tak_toe/TikTakToe.java
+++ src/com/bz/tek_tak_toe/TikTakToe.java
@@ -1,7 +1,12 @@
 package com.bz.tek_tak_toe;
 
+import java.util.Scanner;
+
 public class TikTakToe {
+	static char userChoice;
+	static char computerChoice;
 	char[] board = new char[10];
+	Scanner sc = new Scanner(System.in);
 
 	public void CreateBoard() {
 
@@ -10,10 +15,34 @@ public class TikTakToe {
 		}
 	}
 
+	public void ChooseLetter() {
+		System.out.println("Choose your point \n 1 for 'x' or choose 2 for 'o'");
+		int option = sc.nextInt();
+
+		switch (option) {
+
+		case 1:
+			userChoice = 'X';
+			computerChoice = 'o';
+			System.out.println("user choice is:" + userChoice + " computer choice is:" + computerChoice);
+			break;
+		case 2:
+			userChoice = 'X';
+			computerChoice = 'o';
+			System.out.println("user choice is:" + userChoice + " computer choice is:" + computerChoice);
+			break;
+		default:
+			System.out.println("invalid input");
+			ChooseLetter();
+		}
+
+	}
+
 	public static void main(String[] args) {
 		System.out.println("Wel Come to Tik_Tak_Toe");
 		TikTakToe obj = new TikTakToe();
 		obj.CreateBoard();
+		obj.ChooseLetter();
 	}
 
 }
